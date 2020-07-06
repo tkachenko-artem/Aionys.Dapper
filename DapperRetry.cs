267818b4babb61a969cddf97270b7c9da2f56011
tkachenko-artem <artem.tkachenko@aionys.com>
2020-07-06 16:48:21 +0300

    Added retry overloads with new connections

--- DapperRetry.cs
+++ DapperRetry.cs
@@ -38,10 +38,7 @@ namespace Aionys.Dapper
             int? commandTimeout = null,
             CommandType? commandType = null)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                return await QueryAsync<T>(db, sql, parameters, retryLimit, transaction, commandTimeout, commandType);
-            }
+            return await RetryActionAsync(QueryNewConnectionAsync<T>(connectionString, sql, parameters, transaction, commandTimeout, commandType), retryLimit);
         }
 
         public async Task<IEnumerable<T>> QueryAsync<T>(
@@ -67,10 +64,7 @@ namespace Aionys.Dapper
             int? commandTimeout = null,
             CommandType? commandType = null)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                return await QueryFirstOrDefaultAsync<T>(db, sql, parameters, retryLimit, transaction, commandTimeout, commandType);
-            }
+            return await RetryActionAsync(QueryFirstOrDefaultNewConnectionAsync<T>(connectionString, sql, parameters, transaction, commandTimeout, commandType), retryLimit);
         }
 
         public async Task<T> QueryFirstOrDefaultAsync<T>(
@@ -96,10 +90,7 @@ namespace Aionys.Dapper
             int? commandTimeout = null,
             CommandType? commandType = null)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                return await ExecuteAsync(db, sql, parameters, retryLimit, transaction, commandTimeout, commandType);
-            }
+            return await RetryActionAsync(ExecuteNewConnectionAsync(connectionString, sql, parameters, transaction, commandTimeout, commandType), retryLimit);
         }
 
         public async Task<int> ExecuteAsync(
@@ -128,6 +119,48 @@ namespace Aionys.Dapper
             }));
 
             return result;
+        }
+
+        public async Task<int> ExecuteNewConnectionAsync(
+            string connectionString,
+            string sql,
+            object parameters = null,
+            IDbTransaction? transaction = null,
+            int? commandTimeout = null,
+            CommandType? commandType = null)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return await db.ExecuteAsync(sql, parameters, transaction, commandTimeout, commandType);
+            }
+        }
+
+        public async Task<T> QueryFirstOrDefaultNewConnectionAsync<T>(
+            string connectionString,
+            string sql,
+            object parameters = null,
+            IDbTransaction? transaction = null,
+            int? commandTimeout = null,
+            CommandType? commandType = null)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return await db.QueryFirstOrDefaultAsync<T>(sql, parameters, transaction, commandTimeout, commandType);
+            }
+        }
+
+        private async Task<IEnumerable<T>> QueryNewConnectionAsync<T>(
+            string connectionString,
+            string sql,
+            object parameters = null,
+            IDbTransaction? transaction = null,
+            int? commandTimeout = null,
+            CommandType? commandType = null)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return await db.QueryAsync<T>(sql, parameters, transaction, commandTimeout, commandType);
+            }
         }
     }
 }
